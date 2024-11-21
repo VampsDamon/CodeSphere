9a7b91252781514727c052bce6ac33c7a1af5889
Shahid Khan <shahidseran786@gmail.com>
2024-11-22 02:05:57 +0530

    Current Month Set Up Done

--- frontend/src/components/Admin/Dashboard/Chart.jsx
+++ frontend/src/components/Admin/Dashboard/Chart.jsx
@@ -9,12 +9,11 @@ import {
   Tooltip,
   ArcElement,
   Legend,
-  plugins
+  plugins,
 } from "chart.js";
 
-import { Line,Doughnut } from "react-chartjs-2";
+import { Line, Doughnut } from "react-chartjs-2";
 import { position } from "@chakra-ui/react";
-
 
 ChartJS.register(
   CategoryScale,
@@ -27,36 +26,65 @@ ChartJS.register(
   Legend
 );
 
-export const LineChart = () => {
-  const labels=["abc",'abc2','abc3','abc4','abc5'];
-  const options={
-    responsive:true,
-    plugins:{
-      legend:{
-        position:"bottom"
-      },
-      title:{
-        display:true,
-        text:"Yearly Views"
-      }
+
+function getLastMonth () {
+    const currentMonth = new Date().getMonth();
+    const labels = [];
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    for (let i = currentMonth; i < months.length; i++) {
+      labels.push(months[i]);
     }
-  }
-  const data={
-    labels,
-    datasets:[
-      {
-        label:"Views",
-        data:[1,2,3,4,5],
-        borderColor:"rgba(107,70,193,0.5)",
-        backgroundColor:"#6b46c1"
-      }
-    ]
+    for (let i = 0; i < currentMonth; i++) {
+      labels.push(months[i]);
+    }
+
+    return labels
   };
-  return <Line options={options} data={data}  />
+ 
+
+export const LineChart = () => {
+  const labels = getLastMonth();
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Yearly Views",
+      },
+    },
+  };
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Views",
+        data: [12, 22, 31, 4, 5,1,23,5,2,24,3,25],
+        borderColor: "rgba(107,70,193,0.5)",
+        backgroundColor: "#6b46c1",
+      },
+    ],
+  };
+  return <Line options={options} data={data} />;
 };
 
-
-export const DoughnutChart=()=>{
+export const DoughnutChart = () => {
   const data = {
     labels: ["Subscribed", "Not Subscribed"],
     datasets: [
@@ -65,12 +93,11 @@ export const DoughnutChart=()=>{
         data: [3, 20],
         borderColor: ["rgb(62,12,171)", "rgba(214,43,129"],
         backgroundColor: ["rgba(62,12,171,0.3)", "rgba(214,43,129,0.3)"],
-        borderWidth:1
+        borderWidth: 1,
       },
     ],
   };
 
-
-  return <Doughnut  data={data}/>
-}
-
+  
+  return <Doughnut data={data} />;
+};
