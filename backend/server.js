8866bbc06853c2b54626ea6fc4bcf8e7b16270c1
Shahid Khan <shahidseran786@gmail.com>
2024-11-25 23:47:42 +0530

    DB setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
+import { connectionDB } from "./config/database.js";
 
 const PORT=process.env.PORT
+
+connectionDB();
 app.listen(PORT,()=>{
     console.log("Server is working on PORT :",PORT);
 })
