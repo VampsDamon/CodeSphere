d32146874ae639341b11b74e078a929c9bdcb720
Shahid Khan <shahidseran786@gmail.com>
2024-12-24 00:03:37 +0530

    DB URL Updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectionDB=async ()=>{
-    const {connection} =await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected with ${connection.host}`);
+    
+    try {
+        const {connection} =await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected with ${connection.host}`);
+    } catch (error) {
+        console.log("Error is Connection with DB : ",error)
+    }
 }
 
